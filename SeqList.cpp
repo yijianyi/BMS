aa9112858b36fe3b0ea18b01c7ef4d9b3a36edab
yjy <wyyangxueqian@163.com>
2023-10-09 20:46:00 +0800

    加了点注释

--- SeqList.cpp
+++ SeqList.cpp
@@ -3,6 +3,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/*
+* @brief 创建动态数组
+* @param books BookArray类型的引用
+* @retval 无
+*/
 void createBookArray(BookArray& books)
 {
 	books.books = (Book*)malloc(sizeof(Book) * initSize);
@@ -10,6 +15,12 @@ void createBookArray(BookArray& books)
 	books.size = 0;
 }
 
+/*
+* @brief 动态数组扩容
+* @param books BookArray类型的引用
+* @param inflateSize 扩展的容量，要扩展五个位置就为5
+* @retval 返回是否扩容成功，成功为true，失败为false
+*/
 bool inflateArray(BookArray& books, size_t inflateSize)
 {
 	Book* temp = (Book*)realloc(books.books, (books.capacity + inflateSize) * sizeof(Book));
