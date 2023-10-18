78003a436ece6a65e05ddf8aed342855e4a379a2
thirdh6o <thirdh6o@foxmail.com>
2023-10-18 23:29:03 +0800

    入库部分代码

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -33,4 +33,24 @@ void max(LinkedList& books)
 	}
 	std::cout << "最贵的图书是："<<maxBook.name << "价值为："<< max <<"元"<< std::endl;
 }
+/*
+*@brief 入库操作
+*/
+void insertBook(LinkedList& books){
 
+	Book book;
+	int count;
+	std::cin >>count;
+	std::cout << "请输入图书ID,图书名称,图书价格,中间以空格相隔" << std::endl;
+	LinkedList iter = books;
+	for(int i=0;i<count;i++){
+		std::cin >> book.ID >> book.name >> book.price;
+		iter->book = book;
+		iter=iter->next;
+	}
+	std::cout << "输入插入位置" << std::endl;
+	std::cin>>count;
+	for (int i = 0; i < count; i++){
+		iter=iter->next;
+	}
+}
